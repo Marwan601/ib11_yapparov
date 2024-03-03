40d7c6aebc35ac7e2deda15e86d6f1144f133faf
yapparov_dilnar <dilnar2014@yandex.ru>
2024-03-03 16:39:05 +0500

    00

--- 00/06.py
+++ 00/06.py
@@ -1,1 +1,6 @@
-text = input()
+text1 = input()
+text2 = input()
+text3 = input()
+print(text1)
+print(text2)
+print(text3)
---
+++ 00/07.py
@@ -0,0 +1,4 @@
+move = input()
+mest = input()
+time = input()
+print('билет на',  '"' + move + '"', 'в', '"' + mest + '"', 'на', time, 'забронирован.')
