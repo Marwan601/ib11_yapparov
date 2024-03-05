ce9cf5f23360c1396479b044d837e3a5c28dcf30
LenarAndElvina <lenar.shamsiyarov.04@gmail.com>
2024-03-05 09:14:45 +0500

    2

---
+++ 01/03.py
@@ -0,0 +1,10 @@
+one = input()
+two = input()
+three = input()
+if one == "раз":
+    if two == "два":
+        if three == "три":
+            print("ГОРИ")
+        else:
+            print("не гори")
+
---
+++ 01/04.py
@@ -0,0 +1,9 @@
+one = input()
+two = input()
+three = input()
+if one == "раз" or "1":
+    if two == "два" or "2":
+        if three == "три" or "3":
+            print("ГОРИ")
+        else:
+            print("не гори")
